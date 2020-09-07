ad408e3da8adbd67532cfe27ff5609c6ca8bd5a5
kimx5230 <kdaustin94@gmail.com>
2020-09-06 17:12:27 -0700

    Updates

---
+++ precache-manifest.261bb9fdca4aea50712ea6a56485b9b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8060f03b9f854e0e0fe33c3f9dc90c",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "330e46fa6359a1f8b4cc",
+    "url": "/reactportfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "20986c247d1ed4b31199",
+    "url": "/reactportfolio/static/js/2.28b9ba7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactportfolio/static/js/2.28b9ba7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "330e46fa6359a1f8b4cc",
+    "url": "/reactportfolio/static/js/main.7a1a20c0.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactportfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
